b4c44ae908bcfeab2122bbc98c65cfadbf3b5318
Luciano Rafael Flores <lucianorafaelflores@gmail.com>
2026-05-07 23:25:13 -0300

    Modificacion del primer test de navegacion para seguir el framework POM

--- tests/recruitment/navegation-recruiment-test.spec.js
+++ tests/recruitment/navegation-recruiment-test.spec.js
@@ -1,6 +1,12 @@
 import { test, expect } from '@playwright/test';
+// 1. Traemos nuestro "molde" del archivo que creamos
+const { RecruitmentPage } = require('../../pages/RecruitmentPage');
+
 
 test('test', async ({ page }) => {
+// Creacion de la instancia de la clase recruitmentPage
+  const recruitmentPage = new RecruitmentPage(page);
+
 // ARRANGE
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
   await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
@@ -8,12 +14,16 @@ test('test', async ({ page }) => {
   await page.getByRole('button', { name: 'Login' }).click();
 
   // ACT
-  await page.getByRole('link', { name: 'Recruitment' }).click();
+  //En lugar de buscar el link aquí, le pedimos al objeto que lo haga
+  await recruitmentPage.goToRecruitment();
 
   // ASSERT
   // 1. Validamos que el heading sea visible
   await expect(page.getByRole('heading', { name: 'Recruitment' })).toBeVisible();
-
+  
   // 2. Validamos la URL usando una expresión regular
-  await expect(page).toHaveURL(/.*recruitment.*/);
+  // los expect no se ponen denttro del archivo de la pagina
+  // RecruitmentPage.js sino que se queda en el archivo del Test
+  // Esta es una pequeña prueba para el POO
+  await expect(page).toHaveURL(/.*\/recruitment/);  
 });
